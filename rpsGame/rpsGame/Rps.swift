648066557599b4c6cf19cc7f9d8d7f48d82b2b2a
sohee <sh663746@kpcard.co.kr>
2025-06-23 18:03:53 +0900

    코드 작성

---
+++ rpsGame/rpsGame/Rps.swift
@@ -0,0 +1,15 @@
+//
+//  Rps.swift
+//  rpsGame
+//
+//  Created by KPC-N-0036 on 6/23/25.
+//
+
+import Foundation
+
+enum Rps :Int {
+    //원시값 (raw lable)
+    case rock
+    case paper
+    case scissor
+}
--- rpsGame/rpsGame/ViewController.swift
+++ rpsGame/rpsGame/ViewController.swift
@@ -19,8 +19,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var myChoiceLable: UILabel!
     
-    
-    
+    var myChoice: Rps = .rock
+    var comChoice: Rps = Rps(rawValue: Int.random(in: 0...2))!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +40,56 @@ class ViewController: UIViewController {
     //함수 하나에 3개 연결함
     @IBAction func rpsButtonTapped(_ sender: UIButton) {
         //가위 바위 보를 선택해서 그 정보를 저장해야함.
+        //현재 버튼에 표시 되고 있는 title을 알려줌..
+     let title =  sender.currentTitle!
+        
+        switch title {
+        case "가위" :
+            myChoice = Rps.scissor
+        case "바위" :
+            myChoice = Rps.rock
+        case "보" :
+            myChoice = Rps.paper
+        default : break
+ 
+        }
+     
+        
         
     }
     
     @IBAction func selectButtonTapped(_ sender: UIButton) {
+        
+        switch comChoice {
+        case .rock :
+            comImageView.image = #imageLiteral(resourceName: "rock")
+            comChoiceLable.text = "바위"
+        case .paper : 
+            comImageView.image = #imageLiteral(resourceName: "paper")
+            comChoiceLable.text = "보"
+        case .scissor : 
+            comImageView.image = #imageLiteral(resourceName: "scissors")
+            comChoiceLable.text = "가위"
+        }
+        switch myChoice {
+        case .rock :
+            comImageView.image = #imageLiteral(resourceName: "ready")
+            comChoiceLable.text = "바위"
+        case .paper :
+            comImageView.image = #imageLiteral(resourceName: "paper")
+            comChoiceLable.text = "보"
+        case .scissor :
+            comImageView.image = #imageLiteral(resourceName: "scissors")
+            comChoiceLable.text = "가위"
+        }
+        
+        if(mychoice = Rps.rock) {
+            comChoice = Rps.paper
+            
+        }
+        
+        
+        
         //1) 컴퓨터가 랜덤 선택한 걸 이미지뷰에 표시
         //2) 컴퓨터 레이블에 표시
         //3) 내가 선택한거 이미지뷰 표시
@@ -61,4 +107,3 @@ class ViewController: UIViewController {
     }
     
 }
-
