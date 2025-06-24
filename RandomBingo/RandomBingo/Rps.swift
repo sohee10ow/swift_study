a41a4dea34da8d18ca908960b0aeba6deb06577a
sohee <sh663746@kpcard.co.kr>
2025-06-24 09:28:08 +0900

    macos 연습 프로젝트 init

---
+++ RandomBingo/RandomBingo/Rps.swift
@@ -0,0 +1,8 @@
+//
+//  Rps.swift
+//  RandomBingo
+//
+//  Created by KPC-N-0036 on 6/24/25.
+//
+
+import Foundation
---
+++ RandomBingo/RandomBingo/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  RandomBingo
+//
+//  Created by KPC-N-0036 on 6/24/25.
+//
+
+import Foundation
+
+print("Hello, World!")
+
